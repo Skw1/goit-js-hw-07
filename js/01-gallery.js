d66ee36859e451845d302235fec77101a4cad7df
Andrii Doluda <andrewdoluda@gmail.com>
2023-12-18 02:43:10 +0100

    Task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const createGalleryItem = ({ preview, original, description }) => {
+  return `  <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                     <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+                </a>
+            </li>`;
+};
+
+const galleryMarkup = galleryItems.map(createGalleryItem).join('');
+galleryContainer.innerHTML = galleryMarkup;
+
+galleryContainer.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  const { nodeName, dataset } = event.target;
+
+  if (nodeName !== 'IMG') {
+    return;
+  }
+
+  const largeImageURL = dataset.source;
+
+  const instance = basicLightbox.create(`
+        <img src="${largeImageURL}" alt="big-photo">
+      `);
+
+  instance.show();
+}
